91c7c57832219f8b8caa234a264c0a2d0234bc96
Darth Spectra <LydiaMarieWilliamson@gmail.com>
2022-08-29 18:55:56 -0500

    Snapshot of Chapter 48 from D. Bolton's "LearnC".

---
+++ lib.h
@@ -0,0 +1,12 @@
+int Random(int max);
+void LogError(char * msg);
+void LogError2(const char * msg1, const char * msg2);
+
+// Logging functions 
+void l(char * loc);
+void l2(char * loc, char * msg);
+void ln(char * loc, char * msg, int n);
+char * sltoa(int n);
+void InitLogging(char * filename);
+void CloseLogging();
+
