e04fc405d3afbc205036b564f8c1f7e924221d7e
Darth Spectra <LydiaMarieWilliamson@gmail.com>
2022-08-29 19:36:09 -0500

    Layout Normalization I: Indentation & trimming.

--- lib.c
+++ lib.c
@@ -1,64 +1,63 @@
 #ifndef lib
-#include <stdio.h>
-#include <stdlib.h>
-#include "lib.h"
-#include <windows.h>
-#include <tchar.h>
-#include <stdio.h>
-#define lib
+#   include <stdio.h>
+#   include <stdlib.h>
+#   include "lib.h"
+#   include <windows.h>
+#   include <tchar.h>
+#   include <stdio.h>
+#   define lib
 #endif
 
 int errorCount = 0;
-FILE * dbf;
+FILE *dbf;
 char fbuff[100];
 char ibuff[20];
 char convbuff[20];
 
-// returns a number between 1 and max 
+// returns a number between 1 and max
 int Random(int max) {
-	return (rand() % max) + 1;
+   return (rand()%max) + 1;
 }
 
 // Log Single errors
-void LogError(char * msg) {
-	FILE * err;
-	int error = fopen_s(&err, "errorlog.txt", "a");
-	printf("%s\n", msg);
-	fclose(err);
-	errorCount++;
+void LogError(char *msg) {
+   FILE *err;
+   int error = fopen_s(&err, "errorlog.txt", "a");
+   printf("%s\n", msg);
+   fclose(err);
+   errorCount++;
 }
 
 // Log Errors with two parameters
-void LogError2(const char * msg1, const char * msg2) {
-	FILE * err;
-	int error = fopen_s(&err, "errorlog.txt", "a");
-	fprintf_s(err, "%s %s\n", msg1, msg2);
-	fclose(err);
-	errorCount++;
+void LogError2(const char *msg1, const char *msg2) {
+   FILE *err;
+   int error = fopen_s(&err, "errorlog.txt", "a");
+   fprintf_s(err, "%s %s\n", msg1, msg2);
+   fclose(err);
+   errorCount++;
 }
 
-void l2(char * loc, char * msg) {
-	//fprintf_s(dbf, "%s,%s\n", loc, msg);
+void l2(char *loc, char *msg) {
+//fprintf_s(dbf, "%s,%s\n", loc, msg);
 }
 
-void l(char * loc) {
-	//fprintf_s(dbf, "%s\n", loc);
+void l(char *loc) {
+//fprintf_s(dbf, "%s\n", loc);
 }
 
-void ln(char * loc, char * msg, int n) {
-	//fprintf_s(dbf, "%s,%s,%s\n", loc, msg, sltoa(n));
+void ln(char *loc, char *msg, int n) {
+//fprintf_s(dbf, "%s,%s,%s\n", loc, msg, sltoa(n));
 }
 
-char * sltoa(int n) {
-	_ltoa_s( n, convbuff, sizeof(convbuff),10);
-	return convbuff;
-} 
+char *sltoa(int n) {
+   _ltoa_s(n, convbuff, sizeof(convbuff), 10);
+   return convbuff;
+}
 
-void InitLogging(char * filename) {
-	int error = fopen_s(&dbf, "biglog.txt", "wt");
+void InitLogging(char *filename) {
+   int error = fopen_s(&dbf, "biglog.txt", "wt");
 }
 
 void CloseLogging() {
-	fclose(dbf);
+   fclose(dbf);
 }
-
--- lib.h
+++ lib.h
@@ -1,12 +1,11 @@
 int Random(int max);
-void LogError(char * msg);
-void LogError2(const char * msg1, const char * msg2);
+void LogError(char *msg);
+void LogError2(const char *msg1, const char *msg2);
 
-// Logging functions 
-void l(char * loc);
-void l2(char * loc, char * msg);
-void ln(char * loc, char * msg, int n);
-char * sltoa(int n);
-void InitLogging(char * filename);
+// Logging functions
+void l(char *loc);
+void l2(char *loc, char *msg);
+void ln(char *loc, char *msg, int n);
+char *sltoa(int n);
+void InitLogging(char *filename);
 void CloseLogging();
-
